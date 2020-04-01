aaa93f1300651d04460283e4e40f68002dbf382e
Pedro Wanderley <peu-zico>
2020-03-31 23:38:50 -0300

    axios and sign up

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 
 import { 
   Header, 
@@ -22,20 +24,56 @@ import {
 
 function Main() {
   const [ openModal, setOpenModal ] = useState(false);
+  const [ name, setName ] = useState();
+  const [ locale, setLocale ] = useState();
+  const [ numberMembers, setNumberMembers ] = useState();
 
   function turnOnOff() {
     setOpenModal(!openModal);
+  } 
+
+  async function getChurces() {
+    try {
+      const response = await api.get('/v1/churces');
+
+      if (response.status === 200) {
+        console.log(response.data)
+      }
+    } catch ({ response }) {
+      console.log(response, ' ERROR')
+    }
   }
 
-  return (
+  async function handleSubmit() {
+    try {
+      const response = await api.post('/v1/churces', {
+        name,
+        locale,
+        numberMembers
+      });
+
+      if (response.status === 200) {
+        console.log('RES: ', response.data);
+      }
+
+    } catch({response}) {
+      console.log(response, ' ERROR');
+    }
+  }
+
+  useEffect(() => {
+    getChurces()
+  }, [])
+
+   return (
     <>
       {openModal &&
       <ModalArea props={openModal}>
         <ModalTitle> Cadastrar igreja </ModalTitle>
-          <ModalInput placeholder="Nome da igreja" />
-          <ModalInput placeholder="Onde ela está localizada ?" style={{marginTop: 100}} />
-          <ModalInput placeholder="Informe o número de membros" style={{marginTop: 150}} />
-          <ModalButton>
+          <ModalInput onChange={ e => setName(e.target.value) } placeholder="Nome da igreja" />
+          <ModalInput onChange={ e => setLocale(e.target.value) } placeholder="Onde ela está localizada ?" style={{marginTop: 100}} />
+          <ModalInput onChange={ e => setNumberMembers(e.target.value) } placeholder="Informe o número de membros" style={{marginTop: 150}} />
+          <ModalButton type="submit" onClick={() => handleSubmit()}>
             Salvar
           </ModalButton>
         <ModalClose onClick={ () => turnOnOff() }>
